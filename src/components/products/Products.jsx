41f5c2cdc6e6dff48c78c1bb83dfbe479d892498
Divy <divyparekh1810@gmail.com>
2022-07-27 22:26:23 +0530

    Products component is done

---
+++ src/components/products/Products.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Grid } from "@mui/material";
+import Product from "./product/Product";
+const Products = () => {
+  const products = [
+    {
+      id: 1,
+      name: "shoes",
+      description: "Running Shoes",
+      price: "$100",
+      image:
+        "https://images.pexels.com/photos/19090/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 2,
+      name: "shoes",
+      description: "Running Shoes",
+      price: "$10",
+      image:
+        "https://images.pexels.com/photos/19090/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+  ];
+  return (
+    <main>
+      <Grid container justifyContent="center" spacing={4}>
+        {products.map((product) => (
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  );
+};
+
+export default Products;
---
+++ src/components/products/product/Product.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  IconButton,
+} from "@mui/material";
+
+import useStyles from "./styles";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+
+const Product = ({ product }) => {
+  const classes = useStyles();
+  return (
+    <Card className={classes.root}>
+      <CardMedia
+        className={classes.media}
+        image={product.image}
+        title={product.name}
+      />
+      <CardContent>
+        <div className={classes.cardContent}>
+          <Typography variant="h5" gutterBottom>
+            {product.name}
+          </Typography>
+          <Typography variant="h5">{product.price}</Typography>
+        </div>
+        <Typography variant="body2" color="textSecondary">
+          {product.description}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing className={classes.cardActions}>
+        <IconButton aria-label="Add to Cart">
+          <AddShoppingCartIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default Product;
