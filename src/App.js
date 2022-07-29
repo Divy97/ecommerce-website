0f32cd2f249213dd39ecb8123b9dce896a921e09
Divy <divyparekh1810@gmail.com>
2022-07-29 21:55:00 +0530

    Cart and cartItem is done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import React, { useState, useEffect } from "react";
 import { commerce } from "./lib/commerce";
-import { Products, Navbar } from "./components";
+import { Products, Navbar, Cart } from "./components";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 const App = () => {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
 
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
     setProducts(data);
   };
 
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  };
+
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+    setCart(item);
+  };
   useEffect(() => {
     fetchProducts();
+    fetchCart();
   }, []);
 
   console.log("PRODUCTS", products);
+  console.log(cart);
   return (
     <>
-      <Navbar />
-      <Products products={products} />
+      <Router>
+        <Navbar totalItems={cart.total_items} />
+        <Switch>
+          <Route exact path="/cart">
+            <Cart cart={cart} />
+          </Route>
+          <Route exact path="/">
+            <Products products={products} onAddToCart={handleAddToCart} />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 };
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,7 +12,7 @@ import { ShoppingCart } from "@material-ui/icons";
 import logo from "../../assets/commerce.png";
 import useStyles from "./styles";
 
-const Navbar = () => {
+const Navbar = ({ totalItems }) => {
   const classes = useStyles();
 
   return (
@@ -31,7 +31,11 @@ const Navbar = () => {
           <div className={classes.grow} />
           <div className={classes.button}>
             <IconButton aria-label="Show cart Items" color="inherit">
-              <Badge badgeContent={2} color="secondary" overlap="rectangular">
+              <Badge
+                badgeContent={totalItems}
+                color="secondary"
+                overlap="rectangular"
+              >
                 <ShoppingCart />
               </Badge>
             </IconButton>
