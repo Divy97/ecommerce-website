d340f2597bc52bd6ff93751ce044891930d50155
Divy <divyparekh1810@gmail.com>
2022-07-29 10:05:33 +0530

    Products Fetched from commerce.js

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -2,27 +2,13 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 
 import Product from "./product/Product";
-const Products = () => {
-  const products = [
-    {
-      id: 1,
-      name: "shoes",
-      description: "Running Shoes",
-      price: "$100",
-      image:
-        "https://images.pexels.com/photos/19090/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-      id: 2,
-      name: "shoes",
-      description: "Running Shoes",
-      price: "$10",
-      image:
-        "https://images.pexels.com/photos/19090/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-  ];
+
+import useStyles from "./styles";
+const Products = ({ products }) => {
+  const classes = useStyles();
   return (
-    <main>
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
       <Grid container justifyContent="center" spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
